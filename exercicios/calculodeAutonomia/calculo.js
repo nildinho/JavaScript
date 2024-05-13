fed15810651c069abac588153a376b441beedfae
Vanildo Lima <nildinho.anahi@hotmail.com>
2024-05-13 14:31:43 -0300

    JavaScript

--- exercicios/calculodeAutonomia/calculo.js
+++ exercicios/calculodeAutonomia/calculo.js
@@ -1,13 +1,15 @@
 function calcularAutonomia() {
-    let KmRodados = document.getElementById('km-percorridos')
-    let inputCombustivelAtual = document.getElementById('combustivel-atual')
-    let resultado = document.getElementById('resultado')
+    let inputKmPercorridos = document.getElementById('km-percorridos');
+    let inputCombustivelAtual = document.getElementById('combustivel-atual');
+    let Resultado = document.getElementById('resultado');
+    let Consumo = document.getElementById('consumo');
 
-    let kmPercorridos = Number(KmRodados.value)
-    let combustivelAtual = Number(inputCombustivelAtual.value)
+    let kmPercorridos = Number(inputKmPercorridos.value);
+    let combustivelAtual = Number(inputCombustivelAtual.value);
 
+    let autonomia = kmPercorridos / combustivelAtual;
+    let consumo = combustivelAtual / kmPercorridos;
 
-    let autonomia = (kmPercorridos / combustivelAtual) 
-
-    resultado.textContent = `A autonomia do veículo é de aproximadamente ${autonomia.toFixed(2)} Quilometros por litro.`
+    Resultado.textContent = `A autonomia do veículo é de aproximadamente ${autonomia.toFixed(2)} Km/l`;
+    Consumo.textContent = `O veículo gasta aproximadamente ${consumo.toFixed(2)} litros por quilômetro.`;
 }
